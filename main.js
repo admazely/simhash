819f637f7d03e36c77f59eb19e785acfdac2d3c2
David Björklund <david.bjorklund@gmail.com>
2012-10-08 23:46:26 +0200

    Fix correct main in package.json

--- main.js
+++ main.js
@@ -7,33 +7,56 @@ function Simhash(algorithm) {
     if (!algorithm) {
         this.algorithm = 'crc32';
     }
-    this.hashLength = this._hashBuffer(new Buffer('')).length;
+    var tmp = this._hashBuffer(new Buffer(''));
+    if (typeof(tmp) === 'number') {
+        this.hashLength = 32;
+    } else {
+        this.hashLength = tmp.length * 8;
+    }
+    this._hashed = [];
 }
 
-Simhash.prototype._accumulate = function(buffers) {
+Simhash.prototype._accumulateNumber = function(input) {
+    
+}
+
+Simhash.prototype._accumulate = function(input) {
     var self = this;
-    var accumulated = new Array(this.hashLength * 8);
-    for(var i = 0; i < accumulated.length; ++i) {
+    var accumulated = new Array(this.hashLength);
+    var i = this.hashLength;
+    while(--i >= 0) {
         accumulated[i] = 0;
     }
-    buffers.forEach(function(buffer) {
-        for(var i = 0; i < self.hashLength * 8; ++i) {
-            var j = i % 8;
-            var k = parseInt(i / 8, 10);
-            var a = (buffer[k] & (1 << j)) ? 1 : -1;
-            accumulated[i] += a;
+    if (typeof(input[0]) === 'number'){
+        i = input.length;
+        while(--i >= 0) {
+            var data = input[i];
+            var j = this.hashLength;
+            while(--j >= 0) {
+                accumulated[j] += (data & (1 << j)) ? 1 : -1;
+            }
         }
-    });
+    } else {
+        i = input.length;
+        while(--i >= 0) {
+            var data = input[i];
+            var j = 0;
+            var l = 3;
+            while(j < self.hashLength) {
+                for(var k = 0; k < 8; ++k) {
+                    accumulated[j] += (data[l] & (1 << k)) ? 1 : -1;
+                    j++;
+                }
+                l--;
+            }
+        };
+    }
     return accumulated;
 }
 
 Simhash.prototype._hashBuffer = function(buffer) {
-    if (!Buffer.isBuffer(buffer)) {
-        buffer = new Buffer(buffer);
-    }
-
     if (this.algorithm === 'crc32') {
-        return crc32(buffer);
+        return crc32.signed(buffer);
     }
 
     var hash = crypto.createHash(this.algorithm);
@@ -42,12 +65,12 @@ Simhash.prototype._hashBuffer = function(buffer) {
 }
 
 Simhash.prototype.hash = function(tokens) {
-    var hashed = new Array(tokens.length);
+    this._hashed.length = tokens.length;
+    // var  hashed = new Array(tokens.length);
     for(var i = 0; i < tokens.length; ++i) {
-        var token = tokens[i];
-        hashed[i] = this._hashBuffer(token);
+        this._hashed[i] = this._hashBuffer(tokens[i]);
     }
-    var result = this._accumulate(hashed);
+    var result = this._accumulate(this._hashed);
     for(var i = 0; i < result.length; ++i) {
         result[i] = result[i] > 0 ? 1 : 0;
     }
